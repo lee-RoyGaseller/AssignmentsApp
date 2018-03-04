5af497d2933e11943f65b006209fe9b229e9101f
Lee-Roy Gaseller <leeroygaseller@gmail.com>
2018-03-03 23:17:46 -0600

    Output the current date-time.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHello, AssignmentsApp!\n");
+        //Output the current date-time.
+        LocalDate today = LocalDate.now();
+        System.out.println("today is "+ today);
+
+
     }
+
+
+
 }
